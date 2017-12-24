5f267dfb81769c6ca0c5d2718f21fd0d17202c4b
melon <integrex@yandex.ru>
2017-12-24 11:09:55 +0500

    add axis and marks

--- drawing-function/drawing-funcion.asm
+++ drawing-function/drawing-funcion.asm
@@ -2,7 +2,7 @@ section .text
 global _start
 
 
-draw_pixel:
+draw_ofst_xxel:
 	push rax
 	push rbx
 	push rcx
@@ -16,15 +16,15 @@ draw_pixel:
 	add rax, rcx
 
 	cmp rax, 1080000
-	jg end_draw_pixel
+	jg end_draw_ofst_xxel
 	cmp rax, 0
-	jl end_draw_pixel
+	jl end_draw_ofst_xxel
     
     ;mov [data + rax], byte 255
     mov [data + rax + 1], byte 255
     ;mov [data + rax + 2], byte 255
 
-end_draw_pixel:
+end_draw_ofst_xxel:
 	pop rdx
 	pop rcx
     pop rbx
@@ -59,7 +59,7 @@ draw_by_dy_lp:
 	mov rdx, qword [buffer]
 	add rdx, rax
 	mov rax, rdx
-	call draw_pixel
+	call draw_ofst_xxel
 	
 	pop rbx
 	pop rax
@@ -99,7 +99,7 @@ draw_by_dx_lp:
 	mov rdx, qword [buffer]
 	add rdx, rbx
 	mov rbx, rdx
-	call draw_pixel
+	call draw_ofst_xxel
 	
 	pop rbx
 	pop rax
@@ -117,7 +117,7 @@ draw_line:
 	push rdi
 	push rsi
 
-	cmp rbx, 599
+	cmp rbx, 601
 	jge cond0
 	jmp check_for_limit
 
@@ -131,7 +131,7 @@ check_for_limit:
 	jmp check_for_adj
 
 cond1:
-	cmp rdx, 599
+	cmp rdx, 601
 	jge end_draw_line
 
 
@@ -187,19 +187,209 @@ end_draw_line:
 compute_function:
 	mov [float_x], rdi
 	fild qword [float_x]
-	fdiv qword [float_y]
-	;fadd qword [one]
+	fdiv qword [float_y] ; scaling by x, 50
+	fsub qword [float_a] ; cetralizing
+	fadd qword [ofst_x]  ; add offset by x from task
 	fcos
-	
-	fdivr qword [float_b]
-
-	fadd qword [float_z]
+	fdivr qword [one]    
+	fsub qword [ofst_y]  ; add offset by y from task
+	fmul qword [float_b] ; scaling by y, 30
+	fadd qword [float_z] ; offset by y, 300
 	fistp qword [float_x]
 	mov rsi, qword [float_x]
 	ret
 
+; x: 345
+; y: 315
+
+draw_one:
+	mov rax, 5
+	add rax, rdi
+	mov rbx, 0
+	add rbx, rsi
+	mov rcx, 5
+	add rcx, rdi
+	mov rdx, 10
+	add rdx, rsi
+	call draw_line
+
+	mov rax, 0
+	add rax, rdi
+	mov rbx, 5
+	add rbx, rsi
+	mov rcx, 5
+	add rcx, rdi
+	mov rdx, 0
+	add rdx, rsi
+	call draw_line
+
+	mov rax, 2
+	add rax, rdi
+	mov rbx, 10
+	add rbx, rsi
+	mov rcx, 8
+	add rcx, rdi
+	mov rdx, 10
+	add rdx, rsi
+	call draw_line
+	ret
+
+
+draw_zero:
+	mov rax, 0
+	add rax, rdi
+	mov rbx, 3
+	add rbx, rsi
+	mov rcx, 2
+	add rcx, rdi
+	mov rdx, 0
+	add rdx, rsi
+	call draw_line
+	
+	mov rax, 2
+	add rax, rdi
+	mov rbx, 0
+	add rbx, rsi
+	mov rcx, 5
+	add rcx, rdi
+	mov rdx, 0
+	add rdx, rsi
+	call draw_line
+	
+	mov rax, 5
+	add rax, rdi
+	mov rbx, 0
+	add rbx, rsi
+	mov rcx, 7
+	add rcx, rdi
+	mov rdx, 3
+	add rdx, rsi
+	call draw_line
+	
+	mov rax, 7
+	add rax, rdi
+	mov rbx, 3
+	add rbx, rsi
+	mov rcx, 7
+	add rcx, rdi
+	mov rdx, 8
+	add rdx, rsi
+	call draw_line
+	
+	mov rax, 5
+	add rax, rdi
+	mov rbx, 11
+	add rbx, rsi
+	mov rcx, 7
+	add rcx, rdi
+	mov rdx, 8
+	add rdx, rsi
+	call draw_line
+	
+	mov rax, 5
+	add rax, rdi
+	mov rbx, 11
+	add rbx, rsi
+	mov rcx, 2
+	add rcx, rdi
+	mov rdx, 11
+	add rdx, rsi
+	call draw_line
+	
+	mov rax, 2
+	add rax, rdi
+	mov rbx, 11
+	add rbx, rsi
+	mov rcx, 0
+	add rcx, rdi
+	mov rdx, 8
+	add rdx, rsi
+	call draw_line
+	
+	mov rax, 0
+	add rax, rdi
+	mov rbx, 8
+	add rbx, rsi
+	mov rcx, 0
+	add rcx, rdi
+	mov rdx, 3
+	add rdx, rsi
+	call draw_line
+	
+	ret
 
 _start:
+	mov rax, 300
+	mov rbx, 0
+	mov rcx, 300
+	mov rdx, 600
+	call draw_line
+
+	mov rax, 0
+	mov rbx, 300
+	mov rcx, 600
+	mov rdx, 300
+	call draw_line
+
+	mov rdi, 345
+	mov rsi, 310
+	call draw_one
+
+	mov rdi, 285
+	mov rsi, 265
+	call draw_one
+
+	mov rdi, 287
+	mov rsi, 305
+	call draw_zero
+
+	mov rdi, -5
+
+dx_marks_lp:
+	push rdi
+	mov rsi, rdi
+	mov rdi, 50
+	
+	mov rax, rsi
+	mul rdi
+	add rax, 300
+	mov rdi, rax
+
+	mov rax, rdi
+	mov rbx, 295
+	mov rcx, rdi
+	mov rdx, 305
+	call draw_line
+	pop rdi
+	inc rdi
+	cmp rdi, 6
+	jne dx_marks_lp
+
+	mov rdi, -9
+
+dy_marks_lp:
+	push rdi
+	mov rsi, rdi
+	mov rdi, 30
+	
+	mov rax, rsi
+	mul rdi
+	add rax, 300
+	mov rdi, rax
+
+	mov rax, 295
+	mov rbx, rdi
+	mov rcx, 305
+	mov rdx, rdi
+	call draw_line
+	pop rdi
+	inc rdi
+	cmp rdi, 10
+	jne dy_marks_lp
+
+
+
+
 
 write_to_file:
     mov rdi, filename  ; open file
@@ -263,10 +453,12 @@ section .data
 	float_x: dq 2.0
 	float_y dq 50.0
 	float_z dq 300.0
-	float_a dq 1.0
+	float_a dq 6.0
 	float_b dq 30.0
 	one dq 1.0
 	buffer dq 1.0
 	k dq 0.5
 	delta_x dq 1.0
 	delta_y dq 1.0
+	ofst_x dq 4.7123
+	ofst_y dq 1.5
